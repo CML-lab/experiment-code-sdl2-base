905a4ea40a64337054f3eaa90bf86885ac492447
awong26 <aaron.ld.wong@gmail.com>
2017-11-08 00:11:49 -0500

    Added quad Bezier curves to Path2D, untested

--- Path2D.h
+++ Path2D.h
@@ -13,7 +13,7 @@ class Path2D
 private:
 	GLint nVerts;
 	GLfloat PathWidth;		 //path width, in meters
-	GLfloat Vertices[8][6];  //either 2 pairs of points and a 0 flag (straight line) OR arc center (0-1), radius (2), start angle (3), and arc length (4) with a 1 flag (circular arc - 5).  angles in radians
+	GLfloat Vertices[8][7];  //either 2 pairs of points and flag=0 (straight line - 6) OR arc center (0-1), radius (2), start angle (3), and arc length (4) with flag = 1 (circular arc - 6), angles in radians OR bezier curve P0 (0-1), P1 (2-3), P3 (4-5), flag = 2 (bezier curve - 6)
 	GLfloat color[3];
 	GLint drawOn;
 	GLfloat xcenter;
@@ -23,11 +23,14 @@ public:
 	
 	GLfloat GetPathVert(GLint i, GLint j);		// Gets a requsted polygon vertex
 	GLint GetPathNVerts();						// Gets number of vertices in the path
+	GLfloat GetPathCenterX();				// Gets the x position of the path center
+	GLfloat GetPathCenterY();				// Gets the y position of the path center
+	GLfloat GetPathCenteredVert(GLint i, GLint j);				// Gets the center-adjusted position of the requested path vertex
 		
 	void SetNVerts(GLint sides);				//set the number of sides in the polygon
 	void SetPathWidth(GLfloat width);			// Set path line width
 	void SetOneVert(GLint i, GLint j, GLfloat vert);	// Sets one polygon vertex of the object (in meters)	
-	void SetPathVerts(GLfloat Verts[][6]);		// Sets the polygon vertices of the object (in meters)
+	void SetPathVerts(GLfloat Verts[][7]);		// Sets the polygon vertices of the object (in meters)
 	void SetPathColor(GLfloat clr[]);			// Sets the polygon color
 	void SetPathCenter(GLfloat centerx, GLfloat centery);	// Sets the center of the path
 	
