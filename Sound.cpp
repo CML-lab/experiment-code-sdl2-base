1be206eccb1e90b81721d5f617506bb6dfbf658f
awong26 <aaron.ld.wong@gmail.com>
2019-02-04 17:38:18 -0500

    Added ability to report audio playback status

--- Sound.cpp
+++ Sound.cpp
@@ -3,6 +3,7 @@
 Sound::Sound(char* filePath)
 {
 	chunk = Mix_LoadWAV(filePath);
+	status = -1;
 }
 
 Sound::~Sound()
@@ -17,5 +18,29 @@ Mix_Chunk* Sound::GetChunk() const
 
 void Sound::Play(int loops)
 {
-	Mix_PlayChannel(-1, chunk, loops);
+	channel = Mix_PlayChannel(-1, chunk, loops);
+
+	if (channel == -1)
+	{
+		status = 0;
+		std::cerr << "Audio play error." << std::endl;
+	}
+	else
+		IsPlaying(); //update the current status
+}
+
+int Sound::IsPlaying()
+{
+
+	if (channel > -1)
+		status = Mix_Playing(channel);
+	else //channel not valid, check if any channels are playing
+	{
+		status = Mix_Playing(-1);
+		if (status > 0)
+			status = 1;
+
+	}
+
+	return(status);
 }
--- Sound.h
+++ Sound.h
@@ -3,12 +3,15 @@
 #pragma once
 
 #include "SDL_mixer.h"
+#include <iostream>
 
 // Stores a sound for playback
 class Sound
 {
 private:
 	Mix_Chunk* chunk;
+	int channel;
+	int status;
 public:
 	// filePath is the path to the sound file to use to initialize the Sound
 	Sound(char* filePath);
@@ -20,6 +23,9 @@ public:
 	// Play the sound
 	// loops is the number of times to replay the sound (default 0)
 	void Play(int loops = 0);
+
+	//check if the sound is still playing
+	int IsPlaying();
 };
 
 #endif
